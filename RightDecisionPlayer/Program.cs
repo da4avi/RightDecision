f0596723f5a03198ee0bd017050f0eef0e2706ee
davi <finkler.davi41@gmail.com>
2026-09-03 11:57:59 -0300

    feat player

--- RightDecisionPlayer/Program.cs
+++ RightDecisionPlayer/Program.cs
@@ -1,21 +1,32 @@
+using RightDecisionPlayer.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//banco config
+builder.Services.AddDbConfig(builder.Configuration);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+//swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+//https
 app.UseHttpsRedirection();
 
+//auth
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
